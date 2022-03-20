b2e28b9c035bff4c73ac9c3e742f111e664974ab
Moshe <moisherosenzweig@gmail.com>
2022-03-20 19:03:47 -0400

    commit tree 8b86f5a7995cdb3947b9060d1326e3c1240987d0

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,19 @@
 # Add a console message so we can see output when the seed file runs
+
+
+puts "Making the Magic games"
+
+50.times do 
+
+    Game.create(
+
+        title: Faker::Game.title,
+        genre: Faker::Game.genre,
+        platform: Faker::Game.platform,
+        price: rand(0..60) 
+
+    )
+
+puts "Done With the magic games"
+
+end
